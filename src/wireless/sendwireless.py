107947db0621ed01cfd1d11427a2748494d4f5b6
Mr Dk <562655624@qq.com>
2019-01-11 14:46:57 +0800

    Update comments

--- src/wireless/sendwireless.py
+++ src/wireless/sendwireless.py
@@ -6,11 +6,9 @@
         Use 'send()' to send packets in layer 3.
         Use 'sendp()' to send packets in layer 2.
 
-        The 'iface' for sending should be running at monitor mode.
-
-        The packet needs to be consturcted before sending.
-
-        If the network is down using 'airmon-ng', use 'kismet' instead.
+        The 'iface' for sending should be running at MONITOR mode.
+        The packet needs to be constructed before sending.
+        If the network is down after using 'airmon-ng', use 'kismet' instead.
 '''
 
 from scapy.layers.dot11 import Dot11, Dot11Elt, Dot11Beacon, RadioTap
@@ -23,7 +21,8 @@ dot11.addr1 = "ff:ff:ff:ff:ff:ff"
 dot11.addr2 = "00:c0:ca:7e:a6:42"
 dot11.addr3 = "00:c0:ca:7e:a6:42"
 
-frame = RadioTap()/dot11
+# Constructing packet
+frame = RadioTap() / dot11
 print(frame.summary())
 
 sendp(frame, iface="kismon0", inter=0.1, loop=1)
