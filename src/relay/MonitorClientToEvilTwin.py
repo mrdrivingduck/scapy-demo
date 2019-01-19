255d644bfd44c5e8e980eb3857d974763882e40e
mrdrivingduck <562655624@qq.com>
2019-01-19 14:37:55 +0800

    Relay experiment

--- src/relay/MonitorClientToEvilTwin.py
+++ src/relay/MonitorClientToEvilTwin.py
@@ -5,18 +5,21 @@
         Sniffing the packets from illegal to Evil Twin AP
 '''
 
-import os
+import os, time
 from scapy.layers.dot11 import Dot11, Dot11ProbeResp, LLC
 from scapy.sendrecv import sniff
 
 iface = "kismon0"
-channel = 1
-evilTwinMAC = "00:00:00:00:00:00"
+channel = 11
+victimMAC = "c4:9d:ed:9f:89:d9"
+evilTwinAP = "bc:f6:85:2c:1e:0d"
 
 def callback(packet):
     if packet.haslayer(Dot11):
-        if packet.addr1 == evilTwinMAC:
-            print(packet.summary())
+        if packet.addr2 == victimMAC and packet.addr1 == evilTwinAP:
+            frameType = packet.sprintf("%Dot11.type%")
+            subType = packet.sprintf("%Dot11.subtype%")
+            print(time.time(), frameType, subType, packet.addr2, ">", packet.addr1)
 
 os.system("iwconfig " + iface + " channel " + str(channel))
 sniff(prn=callback, iface=iface)
--- src/relay/MonitorEvilTwinToAP.py
+++ src/relay/MonitorEvilTwinToAP.py
@@ -5,18 +5,21 @@
         Sniffing the packets from illegal Evil Twin AP to legitimate AP
 '''
 
-import os
+import os, time
 from scapy.layers.dot11 import Dot11, Dot11ProbeResp, LLC
 from scapy.sendrecv import sniff
 
-iface = "kismon0"
+iface = "kismon1"
 channel = 1
-realDestination = "00:00:00:00:00:00"
+evilTwinSTAMAC = "00:87:34:50:38:d3"
+realDestiMAC = "94:d9:b3:14:d0:e8"
 
 def callback(packet):
     if packet.haslayer(Dot11):
-        if packet.addr1 == realDestination:
-            print(packet.summary())
+        if packet.addr2 == evilTwinSTAMAC and packet.addr1 == realDestiMAC:
+            frameType = packet.sprintf("%Dot11.type%")
+            subType = packet.sprintf("%Dot11.subtype%")
+            print(time.time(), frameType, subType, packet.addr2, ">", packet.addr1)
 
 os.system("iwconfig " + iface + " channel " + str(channel))
 sniff(prn=callback, iface=iface)
