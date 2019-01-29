cb66b594bc156737174feeb824cbf5a060f1cb89
mrdrivingduck <562655624@qq.com>
2019-01-29 22:48:19 +0800

    Modified manipulation

--- src/manipulation/ManipulationSend.py
+++ src/manipulation/ManipulationSend.py
@@ -5,20 +5,30 @@ import os
 from scapy.layers.dot11 import Dot11, Dot11Elt, Dot11Beacon, RadioTap
 from scapy.sendrecv import send, sendp
 
+SSID = "11223344"
+
+elt = Dot11Elt()
+elt.ID = 0
+elt.info = SSID
+elt.len = len(SSID)
+
+beacon = Dot11Beacon(cap="ESS+privacy")
 
 dot11 = Dot11()
-dot11.type = 2
-dot11.subtype = 4
-dot11.addr1 = "ff:ff:ff:ff:ff:ff"  # destination
-dot11.addr2 = "00:c0:ca:7e:a6:42"  # sender
-dot11.addr3 = "00:c0:ca:7e:a6:42"  # sender
+dot11.type = 0
+# dot11.subtype = 8
+# dot11.addr1 = "12:34:56:78:90:ab"  # destination
+dot11.addr1 = "ff:ff:ff:ff:ff:ff"
+dot11.addr2 = "ba:09:87:65:43:21"  # sender
+dot11.addr3 = "ba:09:87:65:43:21"  # sender
 
 # Constructing packet
-frame = RadioTap() / dot11
+frame = RadioTap() / dot11 / beacon / elt
 print(frame.summary())
 
-iface = "kismon0"
+iface = "kismon1"
 channel = 1
 
 os.system("iwconfig " + iface + " channel " + str(channel))
-sendp(frame, iface=iface, inter=0.1, loop=1, count=1)
+# sendp(frame, iface=iface, inter=0.1, loop=1, count=1)
+sendp(frame, iface=iface)
--- src/manipulation/ManipulationSniff.py
+++ src/manipulation/ManipulationSniff.py
@@ -8,8 +8,10 @@ from scapy.sendrecv import sniff
 
 def callback(packet):
     if packet.haslayer(Dot11):
-        if packet.addr2 == "00:c0:ca:7e:a6:42":
-            print(packet.summary())
+        if packet.addr2 == "ba:09:87:65:43:21" and packet.addr1 == "ff:ff:ff:ff:ff:ff":
+            print(packet.show())
+        # if packet.type == 0 and packet.subtype == 8 and packet.addr2 == "94:d9:b3:db:ba:63":
+        #     print(packet.show())
 
 iface = "kismon0"
 channel = 1
