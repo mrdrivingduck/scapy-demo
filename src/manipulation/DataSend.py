0bae359391c58415fe60fc261746350fcde69ad5
mrdrivingduck <562655624@qq.com>
2019-01-30 21:23:59 +0800

    Support data frame manipulation

--- src/manipulation/DataSend.py
+++ src/manipulation/DataSend.py
@@ -1,36 +1,24 @@
 '''
     @author - mrdrivingduck
     @version - 2019.01.30
-    @function
-        Send a beacon frame with specific element information.
-    @issue
-        A beacon frame will be sniffed continuously.
+    @function - 
+        To send a 802.11 data frame.
 '''
-
 
 import os
 from scapy.layers.dot11 import Dot11, Dot11Elt, Dot11Beacon, RadioTap
 from scapy.sendrecv import send, sendp
 
-SSID = "11223344"
-
-elt = Dot11Elt()
-elt.ID = 0
-elt.info = SSID
-elt.len = len(SSID)
-
-beacon = Dot11Beacon(cap="ESS+privacy", timestamp=100000000, beacon_interval=10000)
-
 dot11 = Dot11()
-dot11.type = 0
-# dot11.subtype = 8
-# dot11.addr1 = "12:34:56:78:90:ab"  # destination
-dot11.addr1 = "ff:ff:ff:ff:ff:ff"
+dot11.type = 2
+dot11.subtype = 0
+dot11.FCfield = "to-DS"
+dot11.addr1 = "12:34:56:78:90:ab"  # destination
 dot11.addr2 = "ba:09:87:65:43:21"  # sender
 dot11.addr3 = "ba:09:87:65:43:21"  # sender
 
 # Constructing packet
-frame = RadioTap() / dot11 / beacon / elt
+frame = RadioTap() / dot11
 print(frame.show())
 
 iface = "kismon1"
--- src/manipulation/DataSniff.py
+++ src/manipulation/DataSniff.py
@@ -1,18 +1,24 @@
 '''
     @author - mrdrivingduck
     @version - 2019.01.30
-    @function
-        To sniff a beacon frame.
+    @function - 
+        To sniff a 802.11 data frame.
+        Filter all retransmission frame.
 '''
 
 import os
 from scapy.layers.dot11 import Dot11, Dot11ProbeResp, Dot11Elt, Dot11Beacon
 from scapy.sendrecv import sniff
 
+count = 0
+
 def callback(packet):
+    global count
     if packet.haslayer(Dot11):
-        if packet.addr2 == "ba:09:87:65:43:21" and packet.addr1 == "ff:ff:ff:ff:ff:ff":
-            print(packet.show())
+        if packet.addr2 == "ba:09:87:65:43:21" and packet.addr1 == "12:34:56:78:90:ab":
+            if not "retry" in packet.sprintf("%Dot11.FCfield%"):
+                print(packet.show(), count)
+                count += 1
 
 iface = "kismon0"
 channel = 1
